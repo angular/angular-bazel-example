601d215cc5335195683bda42ef0d5adbf6f70341
Greg Magolan <gmagolan@gmail.com>
2018-09-10 10:37:50 -0700

    Move AppComponent to AppModuleProd and remove old unused main.ts

--- src/app.module.prod.ts
+++ src/app.module.prod.ts
@@ -5,11 +5,12 @@ import {StoreModule} from '@ngrx/store';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {AppModule} from './app.module';
 import {todoReducer} from './reducers/reducers';
 
 @NgModule({
-  imports: [AppModule, BrowserModule, AppRoutingModule, StoreModule.forRoot({todoReducer})],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({todoReducer})],
+  exports: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModuleProd {
--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-
-import {AppComponent} from './app.component';
-
-@NgModule({declarations: [AppComponent], imports: [RouterModule], exports: [AppComponent]})
-export class AppModule {
-}
