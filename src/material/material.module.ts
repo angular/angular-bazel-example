5c5d52fda6c0da7cf68381bf6036b1254a1e1e3d
Alex Eagle <alexeagle@angular.io>
2019-02-13 15:10:44 -0800

    Switch to deep imports of ANgular Material

--- src/material/material.module.ts
+++ src/material/material.module.ts
@@ -1,5 +1,11 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 const matModules = [
   MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule,
