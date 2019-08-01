32287408938e4b0cf7132803ad2ed429ae308777
Alex Eagle <alexeagle@angular.io>
2019-08-01 16:41:42 -0700

    Update to rollup_bundle with enable_code_splitting
    We no longer need to specify the additional entry points

--- tools/generator/src/clean.js
+++ tools/generator/src/clean.js
@@ -1,6 +1,5 @@
 const rimraf = require('rimraf');
 const { FEATURES } = require('./feature-names');
-const { updateBuildFile } = require('./build-file');
 const { updateNgModule, removeRoutesFromNgModule, removeRoutesFromAppComponentHtml } = require('./ng-module');
 
 module.exports = function() {
@@ -9,8 +8,6 @@ module.exports = function() {
     rimraf.sync(featPath);
   });
 
-  updateBuildFile('src/app/BUILD.bazel', {mappedFeatureList: []});
-  updateBuildFile('src/BUILD.bazel', {mappedFeatureList: []});
   updateNgModule('src/app/app.module.ts');
   removeRoutesFromNgModule('src/app/app-routing.module.ts');
   removeRoutesFromAppComponentHtml('src/app/app.component.html');
